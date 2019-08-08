fc7c0ba86c66e27f4ffbe43db36b2944b3762d0f
JohnGlennon <glenn.geysen@student.kdg.be>
2019-08-08 18:28:38 +0200

    detects color and stops for 5 seconds in front of a red light

--- eindproject.py
+++ eindproject.py
@@ -27,8 +27,10 @@ class Car():
 		# The speed in meters per second
 		self.speed = 0.4
 
+		# Converts image to bgr
 		self.bridge = CvBridge()
 
+		# Subscribe to image topic
 		self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_callback)
 
 		# The counter that counts when a picture needs to be taken
@@ -40,13 +42,16 @@ class Car():
 		self.image = None
 
 		# Set rate to update robot's movement
-		self.r = rospy.Rate(10)
+		self.rate = rospy.Rate(10)
 
 		# Initialize the movement command
 		self.move_cmd = Twist()
 
 		# Publisher to control the robot's movement
 		self.cmd_vel_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=5)
+
+		# Distance to wall
+		self.distance = 0
 
 		# Subscribe to the laserscan
 		self.scan_subscriber = rospy.Subscriber('/scan', LaserScan, self.laserscan_callback)
@@ -61,7 +66,7 @@ class Car():
 	def laserscan_callback(self, msg):
 		self.laser = msg
 
-		dist = n = 0
+		n = 0
 
 		midrange_min = midrange_max = 0
 
@@ -75,23 +80,20 @@ class Car():
 		# Compute average distance out of the middle points, skip NaN
 		for point in msg.ranges[midrange_min:midrange_max]:
 			if not isnan(point):
-				dist += point
+				self.distance += point
 				n += 1
 
 		# If no points, keep dist equal to zero
 		if n:
-			dist /= n
-		rospy.loginfo("dist: %s, nbr of points %s", String(dist), String(n))
-
-		# Move the turtlebot
-		self.move_cmd.linear.x = self.speed
+			self.distance /= n
+		rospy.loginfo("dist: %s, nbr of points %s", String(self.distance), String(n))
 
 	def image_callback(self, data):
 		# Convert image to OpenCV format
 		try:
 			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
-			print(e)
+			rospy.loginfo(e)
 
 		self.image_received = True
 		self.image = cv_image
@@ -101,6 +103,10 @@ class Car():
 
 		while not rospy.is_shutdown():
 
+			# Move the turtlebot
+			self.move_cmd.linear.x = self.speed
+
+			# Only take a picture once in every 10 iterations
 			if self.take_picture_counter == 10:
 				# Take a photo
 
@@ -119,7 +125,7 @@ class Car():
 
 			# Publish the movement command
 			self.cmd_vel_pub.publish(self.move_cmd)
-			self.r.sleep()
+			self.rate.sleep()
 
 	def take_picture(self, img_title):
 		if self.image_received:
@@ -127,25 +133,34 @@ class Car():
 			cv2.imwrite(img_title, self.image)
 
 			img = cv2.imread('photo.jpg', 1)
-			# hsv = cv2.cctColor(img, cv2.COLOR_BGR2HSV)
+			hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-			lower_range = np.array([50, 100, 100], dtype=np.uint8)
-			upper_range = np.array([70, 255, 255], dtype=np.uint8)
+			# Range for lower red
+			lower_red = np.array([0, 120, 70], dtype=np.uint8)
+			upper_red = np.array([10, 255, 255], dtype=np.uint8)
+			mask1 = cv2.inRange(hsv, lower_red, upper_red)
 
-			# mask = cv2.inRange(hsv, lower_range, upper_range)
+			# Range for upper red
+			lower_red = np.array([170, 120, 70], dtype=np.uint8)
+			upper_red = np.array([180, 255, 255], dtype=np.uint8)
+			mask2 = cv2.inRange(hsv, lower_red, upper_red)
 
-			# cv2.imshow('mask', mask)
+			# Generating the final mask to detect red
+			mask = mask1 + mask2
+
 			cv2.destroyAllWindows()
 
-			# if cv2.countNonZero(mask) == 0:
-			# 	rospy.loginfo("Image is black")
-			# 	return False
-			# else:
-			self.move_cmd.linear.x = 0
-			self.move_cmd.angular.z = 0
-			rospy.loginfo("Waits 5 seconds for the red light")
-			return True
+			if cv2.countNonZero(mask) == 0:
+				rospy.loginfo("Image doesn't have the required color")
+			elif self.distance > 1:
+				rospy.loginfo("Color detected but distance is too long")
+			else:
+				rospy.loginfo("Waits 5 seconds for the red light")
+				self.move_cmd.linear.x = 0
+				self.move_cmd.angular.z = 0
+				rospy.sleep(5)
 
+			return True
 		else:
 			return False
 
